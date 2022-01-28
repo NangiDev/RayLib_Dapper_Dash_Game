e904d8e7c43037021fffc1819e75c27b68bbfd83
Joel <j.settrbrg@gmail.com>
2022-01-28 10:37:34 +0100

    Finished the game

--- axe_game.cpp
+++ axe_game.cpp
@@ -1,9 +1,9 @@
-#include <string>
+#include <string.h>
 #include "raylib.h"
 
-const int WIDTH = 350;
-const int HEIGHT = 200;
-const char* TITLE = "My Title of the Game";
+const int WIDTH = 640;
+const int HEIGHT = 320;
+const char *TITLE = "Axe Game";
 const int FPS = 60;
 
 int main()
@@ -11,10 +11,36 @@ int main()
     InitWindow(WIDTH, HEIGHT, TITLE);
 
     int circle_x{175};
-    int circle_y{100};
-    int circle_radius{25};
-    Color circle_color{RED};
+    int circle_y = (int)(HEIGHT / 2);
+    int circle_radius{20};
     int circle_step{10};
+    Color circle_color{BLUE};
+
+    int r_circle_x{circle_x + circle_radius};
+    int l_circle_x{circle_x - circle_radius};
+    int u_circle_y{circle_y - circle_radius};
+    int b_circle_y{circle_y + circle_radius};
+
+    int axe_x = (int)(WIDTH / 2);
+    int axe_y = (int)(HEIGHT / 2);
+    int axe_w{25};
+    int axe_h{70};
+    int axe_step{10};
+    Color axe_color{RED};
+
+    int r_axe_x{axe_x + axe_w};
+    int l_axe_x{axe_x};
+    int u_axe_y{axe_y};
+    int b_axe_y{axe_y + axe_h};
+
+    const char *text_game_over = "Game Over!";
+    int text_size{60};
+    int text_length = (int)strlen(text_game_over) * text_size / 2;
+    int text_x = (int)(WIDTH / 2 - text_length / 2);
+    int text_y = (int)(HEIGHT / 2 - text_size / 2);
+    Color text_color{RED};
+
+    bool collision_with_axe{false};
 
     SetTargetFPS(FPS);
 
@@ -22,22 +48,51 @@ int main()
     {
         BeginDrawing();
         ClearBackground(WHITE);
-        DrawCircle(circle_x, circle_y, circle_radius, circle_color);
-        if (IsKeyDown(KEY_D) && circle_x < WIDTH)
+
+        r_circle_x = circle_x + circle_radius;
+        l_circle_x = circle_x - circle_radius;
+        u_circle_y = circle_y - circle_radius;
+        b_circle_y = circle_y + circle_radius;
+        r_axe_x = axe_x + axe_w;
+        l_axe_x = axe_x;
+        u_axe_y = axe_y;
+        b_axe_y = axe_y + axe_h;
+
+        collision_with_axe = b_axe_y >= u_circle_y &&
+                             u_axe_y <= b_circle_y &&
+                             l_axe_x <= r_circle_x &&
+                             r_axe_x >= l_circle_x;
+
+        if (collision_with_axe)
         {
-            circle_x += circle_step;
+            DrawText(text_game_over, text_x, text_y, text_size, text_color);
         }
-        if (IsKeyDown(KEY_A) && circle_x > 0)
+        else
         {
-            circle_x -= circle_step;
-        }
-        if (IsKeyDown(KEY_W) && circle_y > 0)
-        {
-            circle_y -= circle_step;
-        }
-        if (IsKeyDown(KEY_S) && circle_y < HEIGHT)
-        {
-            circle_y += circle_step;
+            DrawCircle(circle_x, circle_y, circle_radius, circle_color);
+            DrawRectangle(axe_x, axe_y, axe_w, axe_h, axe_color);
+
+            axe_y += axe_step;
+            if (axe_y > HEIGHT - axe_h || axe_y < 0)
+            {
+                axe_step = -axe_step;
+            }
+            if ((IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) && circle_x < WIDTH - circle_radius)
+            {
+                circle_x += circle_step;
+            }
+            if ((IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) && circle_x > circle_radius)
+            {
+                circle_x -= circle_step;
+            }
+            if ((IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)) && circle_y > circle_radius)
+            {
+                circle_y -= circle_step;
+            }
+            if ((IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)) && circle_y < HEIGHT - circle_radius)
+            {
+                circle_y += circle_step;
+            }
         }
         EndDrawing();
     }
